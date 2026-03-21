e449c661df8c14f728a23ee26d332c7aa63afebc
sloncaric-tvz <sloncaric@tvz.hr>
2026-03-21 11:05:20 +0100

    Add files via upload

---
+++ DZ/vjezba_1.php
@@ -0,0 +1,10 @@
+<?php
+    $naslov = "Moj prvi PHP dokument";
+    $autor = "Stjepko Lončarić";
+
+    echo "<h1>$naslov</h1>";
+    echo "<p>Ovu stranicu je izradio <strong>$autor</strong>.</p>";
+    echo "<a href='https://www.reddit.com/' target='_blank'>Posjeti REDDIT.COM</a>";
+?>
+
+<!-- Naziv datoteke: vjezba_1.php -->
